c329f2e77af9caaac250231719c79de8e631324f
Willard Nilges <willardnilges@gmail.com>
2024-09-27 18:13:09 -0400

    More stuff

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from minio.error import S3Error
 from dotenv import load_dotenv
 import logging
 from meshdb_client import MeshdbClient
+from pano import Pano
 from storage import Storage
 #from storage_git import StorageGit
 from storage_minio import StorageMinio
@@ -12,30 +13,37 @@ logging.basicConfig(
 )
 log = logging.getLogger("pano")
 
-def main():
+WORKING_DIRECTORY = "/var/pano"
+
+def main() -> None:
     load_dotenv()
 
-    meshdb = MeshdbClient()
-    #installs = meshdb.get_all_installs()
-    #log.info(installs)
-    panos = meshdb.get_building_panos("015c006f-edb8-49a1-bf3e-8728c867b1ca")
+    pano = Pano()
+
+    panos = pano.meshdb.get_building_panos("015c006f-edb8-49a1-bf3e-8728c867b1ca")
     print(panos)
 
-    return
+    #meshdb = MeshdbClient()
+    ##installs = meshdb.get_all_installs()
+    ##log.info(installs)
+    #panos = meshdb.get_building_panos("015c006f-edb8-49a1-bf3e-8728c867b1ca")
+    #print(panos)
 
-    # Initialize the Minio Bucket
-    storage = StorageMinio()
-    #storage.upload_images({destination_file: source_file})
+    #return
 
-    # Clone and/or sync repo
-    github = StorageGit()
-    github.clone_repo()
-    github.sync_repo()
+    ## Initialize the Minio Bucket
+    #storage = StorageMinio()
+    ##storage.upload_images({destination_file: source_file})
 
-    # Sync GitHub to MinIO
-    sync(github, storage)
+    ## Clone and/or sync repo
+    #github = StorageGit()
+    #github.clone_repo()
+    #github.sync_repo()
 
-def sync(source_storage: Storage, destination_storage: Storage):
+    ## Sync GitHub to MinIO
+    #sync(github, storage)
+
+def sync(source_storage: Storage, destination_storage: Storage) -> None:
     pass
     
 if __name__ == "__main__":
