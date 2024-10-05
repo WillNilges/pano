ca3b1da1b248ecc7ff3658bae3755b6f60f56705
Willard Nilges <willardnilges@gmail.com>
2024-10-05 11:27:49 -0400

    Install number IG

--- src/pano.py
+++ src/pano.py
@@ -10,13 +10,15 @@ class Pano:
 
     # Mocking some kind of upload portal with an array of strings
     # TODO: What about NNs? What about normal install photos?
-    def handle_upload(self, install_number: int, mock_file: str) -> None:
+    def handle_upload(self, install_number: int, file_path: str) -> None:
         building = self.meshdb.get_primary_building_for_install(install_number)
-        # TODO: Handle if we don't have a building yet
+        # TODO: Distinguish between the server shitting and getting passed a bad install #
+        if not building:
+            raise ValueError("Could not find a building associated with that Install #")
 
         # Upload object to S3
         object_name = f"{install_number}/{uuid.uuid4}"
-        self.minio.upload_images({object_name: mock_file})
+        self.minio.upload_images({object_name: file_path})
 
         # Save link to object in MeshDB
         self.meshdb.save_panorama_on_building(building.id, object_name)
