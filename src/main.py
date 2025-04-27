dd59e2e529c3db9cc286fe81e3b1075e48427d33
Will Nilges <willardnilges@gmail.com>
2025-04-27 12:12:38 -0400

    who asked

--- src/main.py
+++ src/main.py
@@ -38,7 +38,10 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_DIRECTORY
 app.config["MAX_CONTENT_LENGTH"] = 100 * 1000 * 1000
 app.config["SECRET_KEY"] = "chomskz"
 
-allowed_origins = {"origins": "http://127.0.0.1:3000"}
+allowed_origins = {"origins": [
+    "http://127.0.0.1:3000",
+    "https://forms.devdb.nycmesh.net",
+]}
 
 CORS(
     app,
