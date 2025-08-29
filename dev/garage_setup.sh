56b340754bd0f6f9d6d93d54b3eb044bbe7c381b
Willard Nilges <willardnilges@gmail.com>
2025-08-28 22:59:21 -0400

    add garage setup script

---
+++ dev/garage_setup.sh
@@ -0,0 +1,13 @@
+
+alias pano_garage="podman exec -it pano-dev_garage_1 /garage"
+
+node_id=$(pano_garage status | tail -n 1 | awk '{print $1}')
+pano_garage layout assign -z dc1 -c 1G $node_id
+pano_garage layout apply --version 1
+pano_garage bucket create pano-bucket
+pano_garage bucket info pano-bucket
+pano_garage key create pano-app-key
+pano_garage bucket allow --read --write --owner pano-bucket --key pano-app-key
+
+echo '=== Done! ==='
+echo 'Paste the Key ID and Secret key into your .env as GARAGE_API_KEY and GARAGE_SECRET'
